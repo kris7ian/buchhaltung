5bf5148c0dafcee235329b86b73c7d8999ed4d35
Kristian <kr.arve@gmail.com>
2015-01-11 00:02:08 +0100

    Second commit! ;)

--- buchhaltung/urls.py
+++ buchhaltung/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'buchaltung2.views.home', name='home'),
+    # url(r'^$', 'buchhaltung2.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^bilanz/', include('bilanz.urls')),
     url(r'^admin/', include(admin.site.urls)),
