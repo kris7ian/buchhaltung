3d156e6e6d8521268f83fb0f53da7e6a58bb7687
Kristian <kr.arve@gmail.com>
2015-05-10 19:30:04 +0200

    Fixed Error in kontoSum.py v2

--- bilanz/templatetags/kontoSum.py
+++ bilanz/templatetags/kontoSum.py
@@ -9,10 +9,9 @@ def kontoSum(konto_id, konto_type):
 
     if konto_type == 'A':
         plus = Buchung.objects.filter(buchung_sollKonto=konto_id)
-        minus = Buchung.objects.filter(buchung_habenKonto=konto_id)
+        minus = Buchung.objects.filter(buchung_habenKonto=konto_id).aggregate(Sum('buchung_amount'))
 
         plus_amount = 0
-        minus_amount = 0
 
         for buchung in plus:
             if buchung.buchung_rate != 1:
@@ -20,22 +19,17 @@ def kontoSum(konto_id, konto_type):
             else:
                 plus_amount += buchung.buchung_amount
 
-        for buchung in minus:
-            if buchung.buchung_rate != 1:
-                minus_amount += buchung.buchung_amount * buchung.buchung_rate
-            else:
-                minus_amount += buchung.buchung_amount
+        if minus['buchung_amount__sum'] == None:
+            minus['buchung_amount__sum'] = 0
 
-
-        return int((plus_amount - minus_amount) * 100) / 100
+        return int((plus_amount - minus['buchung_amount__sum']) * 100) / 100
 
 
     else:
-        plus = Buchung.objects.filter(buchung_habenKonto=konto_id)
+        plus = Buchung.objects.filter(buchung_habenKonto=konto_id).aggregate(Sum('buchung_amount'))
         minus = Buchung.objects.filter(buchung_sollKonto=konto_id)
 
         minus_amount = 0
-        plus_amount = 0
 
         for buchung in minus:
             if buchung.buchung_rate != 1:
@@ -43,13 +37,10 @@ def kontoSum(konto_id, konto_type):
             else:
                 minus_amount += buchung.buchung_amount
 
-        for buchung in plus:
-            if buchung.buchung_rate != 1:
-                plus_amount += buchung.buchung_amount * buchung.buchung_rate
-            else:
-                plus_amount += buchung.buchung_amount
+        if plus['buchung_amount__sum'] == None:
+            plus['buchung_amount__sum'] = 0
 
-        return int((plus_amount - minus_amount) * 100) / 100
+        return int((plus['buchung_amount__sum'] - minus_amount) * 100) / 100
 
 
 register.simple_tag(kontoSum)
